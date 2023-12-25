af6ba347bef752d12fa1d15c865bd5eeeffd8106
MayarALiP <mayarali93@gmail.com>
2023-12-25 17:49:50 +0200

    OOP Task

---
+++ SideTasks/OOP_Report.dart
@@ -0,0 +1,31 @@
+/*
+*     Abstract Classes:
+        Definition: Abstract classes are classes in object-oriented programming that cannot be instantiated on their own. They serve as blueprints for other classes, providing a common interface for a group of related classes. Abstract classes may contain abstract methods (methods without implementation) that must be implemented by their concrete subclasses.
+        Key Points:
+            Cannot be instantiated directly.
+            May contain abstract methods.
+            Intended for providing a common structure to derived classes.
+
+    Static:
+        Definition: In the context of OOP, the term "static" refers to a class-level member (variable or method) that belongs to the class itself rather than to instances of the class. Static members are shared among all instances of the class and can be accessed without creating an instance of the class.
+        Key Points:
+            Belongs to the class, not to instances.
+            Accessed using the class name.
+            Shared among all instances of the class.
+
+    Encapsulation:
+        Definition: Encapsulation is one of the fundamental principles of OOP that involves bundling the data (attributes) and the methods (functions) that operate on the data into a single unit called a class. It helps in restricting access to the internal details of an object and only exposes what is necessary for the outside world.
+        Key Points:
+            Bundling data and methods into a class.
+            Controlling access to the internal details of an object.
+            Enhances modularity and maintainability.
+
+    Polymorphism:
+        Definition: Polymorphism is a concept in OOP that allows objects of different types to be treated as objects of a common type. It enables a single interface to represent different underlying forms (data types or classes). Polymorphism can be achieved through method overloading and method overriding.
+        Key Points:
+            "Poly" means many, "morph" means forms.
+            Allows objects of different types to be treated uniformly.
+            Achieved through method overloading and method overriding.
+*
+* 
+* */
